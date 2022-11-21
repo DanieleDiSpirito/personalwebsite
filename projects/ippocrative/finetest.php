11ccbc801b4245860dee80be598ae89aaf25ed48
DanieleDiSpirito <danieledispirito2004@gmail.com>
2022-11-21 12:53:54 +0100

    Ippocrative | Continua

---
+++ projects/ippocrative/finetest.php
@@ -0,0 +1,66 @@
+<?php
+    session_start();
+    if(!(isset($_SESSION['end']) and $_SESSION['end'] === true)) {
+        die('
+        <html style="background-color: #344060; color: white">
+        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+        <h1 style="text-align: center; font-weight: 400">Non hai finito il test</h1>
+        <div style="text-align: center; font-size: 20px;">Reindirizzamento tra <span id="secondi">3</span></div>
+        <script>
+        setInterval(() => {
+            document.location.href = "home.php";
+        }, 3000);
+        setInterval(() => {
+            document.querySelector("span#secondi").innerHTML = document.querySelector("span#secondi").innerHTML - 1;
+        }, 1000);
+        </script>
+        </html>
+        ');
+    }
+    if(isset($_SESSION['session'])) {
+        $account = json_decode(base64_decode($_SESSION['session']));
+        include 'config.php';
+        $stmt = $mysqli->prepare('UPDATE utenti_ippocrative SET idDomanda = NULL WHERE utenti_ippocrative.email = ?;');
+        $stmt->bind_param('s', $account->email);
+        $stmt->execute();
+    }
+    unset($_SESSION['end']);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fine! 🎉</title>
+</head>
+<body>
+    <style>
+        * {
+            background-color: black;
+        }
+
+        img {
+            position: absolute;
+            width: auto;
+            height: 98%;
+            left: 50%;
+            top: 50%;
+            transform: translate(-50%, -50%);
+        }
+
+        span {
+            color: white;
+            position: absolute;
+            left: 50%;
+            top: 90%;
+            transform: translate(-50%, -50%);
+            font-size: 50px;
+            background-color: transparent;
+        }
+    </style>
+    <img src="https://media3.giphy.com/media/14cDsqOkks6O8U/giphy.gif?cid=ecf05e47mdmdr7tqfdziurfoh8tj3d7206b7h2pr04a950zz&rid=giphy.gif&ct=g">
+    <span>🎉 CONGRATULAZIONI! 🎉</span>
+</body>
+</html>
--- projects/ippocrative/quiz.php
+++ projects/ippocrative/quiz.php
@@ -1,26 +1,70 @@
 <!DOCTYPE html>
 
 <?php
+session_start();
 include 'config.php';
-// $idDomanda = @$_POST['domandaSuccessiva'] || 1;
-$idDomanda = 1;
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  $idDomanda = intval($_POST['id_domanda']);
+
+$error = '';
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+  $idDomanda = 1;
 }
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $idDomanda = intval($_POST['id_domanda']) - 1;
+}
+
 $stmt = $mysqli->prepare('SELECT id, domanda, risposta FROM domande WHERE id=?');
 $stmt->bind_param('i', $idDomanda);
 $stmt->execute();
+
 if ($stmt->bind_result($id, $domanda, $risposta)) {
   $stmt->fetch();
   $stmt->close();
 }
+
 if (!(isset($id) || isset($domanda) || isset($risposta))) {
-  die('Test finito');
+  die('Errore');
 }
 
-// risposte nel db
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  //die(strtoupper(implode($_POST['res'])));
+  $rispostaRaw = strtolower($risposta);
+  $rispostaRaw = str_replace(' ', '', $rispostaRaw);
+  $rispostaRaw = str_replace('-', '', $rispostaRaw);
+  $rispostaRaw = str_replace('\'', '', $rispostaRaw);
+  $rispostaData = strtolower(implode('', $_POST['res']));
+
+  if($rispostaRaw !== $rispostaData) {
+    $error = 'Risposta sbagliata!';
+  } else {
+    if($idDomanda === 36) {
+      session_start();
+      $_SESSION['end'] = true;
+      header('Location: finetest.php');
+    }
+
+    if(isset($_SESSION['session'])) {
+      $account = json_decode(base64_decode($_SESSION['session']));
+      $stmt = $mysqli->prepare('UPDATE utenti_ippocrative SET idDomanda = ? WHERE utenti_ippocrative.email = ?;');
+      $idDomanda += 1;
+      $stmt->bind_param('is', $idDomanda, $account->email);
+      $idDomanda -= 1;
+      $stmt->execute();
+      $stmt->close();
+    }
+
+    $stmt = $mysqli->prepare('SELECT id, domanda, risposta FROM domande WHERE id=?');
+    $idDomanda += 1;
+    $stmt->bind_param('i', $idDomanda);
+    $stmt->execute();
+
+    if ($stmt->bind_result($id, $domanda, $risposta)) {
+      $stmt->fetch();
+      $stmt->close();
+    }
+
+    if (!(isset($id) || isset($domanda) || isset($risposta))) {
+      die('Errore');
+    }
+  }
 }
 
 ?>
@@ -28,6 +72,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <html>
 
 <head>
+  <title>Ippocrative | Quiz</title>
   <meta charset="utf-8">
   <link href="quiz.css" rel="stylesheet" />
   <script src="script.js" type="text/javascript"></script>
@@ -54,12 +99,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
           <?php endif; ?>
         <?php endfor; ?>
       </fieldset>
+      <div id="error"><?=$error?></div>
       <div id="submit">
         <?php if ($idDomanda !== 36) : ?>
-          <button type="submit" name="id_domanda" value="<?= $idDomanda + 1 ?>" class="invia"><i class="bi bi-arrow-right"></i></button>
+          <button type="submit" name="id_domanda" value="<?= $idDomanda + 1 ?>" class="invia" id="row"><i class="bi bi-arrow-right-short"></i></button>
         <?php else : ?>
-          <button type="submit" name="id_domanda" value="submit" class="invia">Invia</button>
+          <button type="submit" name="id_domanda" value="<?= $idDomanda + 1 ?>" class="invia">Invia</button>
         <?php endif; ?>
+        <button type="button" name="hint" value="hint" class="invia" id="row"><i class="bi bi-question"></i></button><span id="punteggio">10/10</span>
       </div>
     </form>
   </main>
